1078ee3d6e42bcb5b0c106cc236e267240fe2000
shubham singh rathore <shubhamsingh5169@gmail.com>
2024-02-25 15:22:12 +0530

    JS

---
+++ script.js
@@ -0,0 +1,19 @@
+const element = document.getElementById('guru');
+element.addEventListener('click',function(){
+    document.body.style.backgroundColor = 'blue';
+});
+
+
+
+let Green = document.getElementById("guru1");
+Green.addEventListener('click',function(){
+    document.body.style.backgroundColor = 'Green';
+});
+
+
+
+let black = document.getElementById("guru2");
+black.addEventListener('click',function(){
+    document.body.style.background = 'black';
+});
+
